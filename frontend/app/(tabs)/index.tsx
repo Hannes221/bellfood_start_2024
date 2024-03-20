2acb1ae34429c267c67e3664131f52af2e32635e
arnestrickmann <115920878+arnestrickmann@users.noreply.github.com>
2024-03-21 00:57:05 +0100

    add basic chat input

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -3,11 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import TextInputComponent from '../../components/TextInput';
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Belly</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <TextInputComponent />
     </View>
   );
 }
