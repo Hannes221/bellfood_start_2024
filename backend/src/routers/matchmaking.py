1ab0ea5351fff61946763c9f978ad760ce127b2a
Hannes221 <hannespfau@gmail.com>
2024-03-21 23:50:21 +0100

    Add improvement matching

--- backend/src/routers/matchmaking.py
+++ backend/src/routers/matchmaking.py
@@ -1,22 +1,16 @@
 from typing import Optional
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 from backend.src.utils.matching_algo import get_best_matches
 
 router = APIRouter()
 
-
-class MatchingBody (BaseModel):
-    user_id: Optional[str]
-    email: Optional[str]
-
-@router.post("/matching")
-async def get_matching(body: MatchingBody):
+@router.get("/matching")
+async def get_matching(user_id: Optional[str] = None, email: Optional[str] = None):
     try:
-        assert body.user_id or body.email 
+        assert user_id or email 
     except AssertionError:
         raise HTTPException(
             status_code=400,
             detail="At least one of user_id or email should be provided"
         )
-    return await get_best_matches(body.user_id, body.email)
+    return await get_best_matches(user_id, email)
