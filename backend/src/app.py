09131d414e68fbd9a720d1aaf99d6e0f4c04ee5e
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-03-21 23:10:49 +0100

    Remove redundand CORS middleware configuration

--- backend/src/app.py
+++ backend/src/app.py
@@ -11,18 +11,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-origins = [
-    "http://localhost:8082"
-]
-
 app.include_router(llm.router)
 app.include_router(interest.router)
 app.include_router(chat.router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
